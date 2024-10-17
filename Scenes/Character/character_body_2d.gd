b1bb30e5a5af2ed99ce038202799998cfa6a9051
Balazs <balzs.gyngysi@gmail.com>
2024-10-17 18:44:40 +0200

    tile map and character started

---
+++ Scenes/Character/character_body_2d.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+const SPEED = 75
+
+func _physics_process(delta):
+	
+	var	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if direction: 
+		self.velocity = direction * SPEED
+	else:
+		velocity = Vector2.ZERO
+		
+	move_and_slide()
