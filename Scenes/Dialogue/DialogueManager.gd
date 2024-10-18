fdf920320d73361abe484bb1b983045d9c2622de
Csaba Ekart <csaba.ekart@gmail.com>
2024-10-18 19:12:18 +0200

    further refactor, rename

--- Scenes/Dialogue/DialogueManager.gd
+++ Scenes/Dialogue/DialogueManager.gd
@@ -1,13 +1,11 @@
 extends Node
 
-@onready var DialogueScene = preload("res://Scenes/Dialogue/dialogue_scene.tscn")
-var dialogue_scene
+@onready var DialogueBoxScene = preload("res://Scenes/Dialogue/dialogue_box.tscn")
+var dialogue_box
 
 var dialogue_lines: Array[String] = []
 var current_line_index: int = 0
-
 var dialogue_box_position: Vector2
-
 var is_dialogue_active: bool = false
 var can_advance_line: bool = false
 
@@ -22,11 +20,11 @@ func start_dialogue(position: Vector2, lines: Array[String]) -> void:
 	is_dialogue_active = true
 	
 func _show_dialogue_box():
-	dialogue_scene = DialogueScene.instantiate()
-	dialogue_scene.finished_displaying.connect(_on_dialogue_finished_displaying)
-	get_tree().root.add_child(dialogue_scene)
-	dialogue_scene.global_position = dialogue_box_position
-	dialogue_scene.display_text(dialogue_lines[current_line_index])
+	dialogue_box = DialogueBoxScene.instantiate()
+	dialogue_box.finished_displaying.connect(_on_dialogue_finished_displaying)
+	get_tree().root.add_child(dialogue_box)
+	dialogue_box.global_position = dialogue_box_position
+	dialogue_box.display_text(dialogue_lines[current_line_index])
 	can_advance_line = false
 	
 func _on_dialogue_finished_displaying():
@@ -36,11 +34,12 @@ func _unhandled_input(event):
 	if (
 		event.is_action_pressed("interact") && is_dialogue_active && can_advance_line
 	):
-		dialogue_scene.queue_free()
+		dialogue_box.queue_free()
 		
 		current_line_index += 1
 		if current_line_index >= dialogue_lines.size():
 			is_dialogue_active = false
 			current_line_index = 0
 			return
+			
 		_show_dialogue_box()
