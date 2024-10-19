0c32aa8cc63bc1f3ae51d404a03a16e9905eecb3
Csaba Ekart <csaba.ekart@gmail.com>
2024-10-19 11:04:47 +0200

    update

--- Scenes/Dialogue/dialogue_box.gd
+++ Scenes/Dialogue/dialogue_box.gd
@@ -9,8 +9,8 @@ var text_to_display: String = ""
 var current_letter_index = 0
 
 var letter_time_seconds: float = 0.03
-var space_time_seconds: float = 0.03
-var punctuation_time_seconds: float = 0.03
+#var space_time_seconds: float = 0.03
+#var punctuation_time_seconds: float = 0.03
 
 signal finished_displaying()
 
@@ -33,25 +33,19 @@ func display_text(text: String) -> void:
 	self.global_position.x -= self.size.x / 2
 	self.global_position.y -= size.y + 24
 	
-	self.text_label.text = ""
-	_display_letter()
+	#self.text_label.text = ""
+	_display_next_letter()
 	
-func _display_letter() -> void:
-	self.text_label.text += self.text_to_display[current_letter_index]
+func _display_next_letter() -> void:
+	# self.text_label.text += self.text_to_display[current_letter_index]
+	self.text_label.visible_characters += 1
 	
-	current_letter_index += 1
-	if current_letter_index >= text_to_display.length():
+	if self.text_label.visible_characters >= text_to_display.length():
 		finished_displaying.emit()
 		return
-	
-	match text_to_display[current_letter_index]:
-		"!", ".", ",", "?":
-			letter_display_timer.start(punctuation_time_seconds)
-		" ":
-			letter_display_timer.start(space_time_seconds)
-		_:
-			letter_display_timer.start(letter_time_seconds)
+		
+	letter_display_timer.start(letter_time_seconds)
 			
 			
 func _on_letter_display_timer_timeout() -> void:
-	_display_letter()
+	_display_next_letter()
--- Scenes/NPC/npc.gd
+++ Scenes/NPC/npc.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var interaction_area: InteractionArea = $InteractionArea
 
-const lines:  Array[String] = [
+const DIALOGUE_LINES:  Array[String] = [
 	"Aasdasdad asd dfghdg erte ! g dfgd +",
 	" asdasd gggdgffd d",
 	" asdas ..... "
@@ -13,15 +13,15 @@ var dialogue_manager: DialogueManager
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interaction_area.interact = Callable(self,"_on_interact")
-	dialogue_manager = DialogueManager.create(global_position, lines)
+	dialogue_manager = DialogueManager.create(global_position, DIALOGUE_LINES)
 	add_child(dialogue_manager)
+	dialogue_manager.dialogue_ended.connect(_on_dialogue_ended)
 	
 func _on_interact():
 	dialogue_manager.start_dialogue()
-	dialogue_manager.dialogue_ended.connect(_on_dialogue_ended)
 	
 func _on_dialogue_ended():
-	print("test")
+	print("dilaogue ended")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
