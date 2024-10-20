9d66779d8d84bbf730374959f2c31390ebf29274
Balazs <balzs.gyngysi@gmail.com>
2024-10-20 23:37:22 +0200

    updated tileset origin

--- Scenes/Screens/Game/game.gd
+++ Scenes/Screens/Game/game.gd
@@ -6,7 +6,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	InteractionManager.set_player(player)
-	dark_mode_switch()
+	#dark_mode_switch()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
