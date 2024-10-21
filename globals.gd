71a8251bce234f705d064665abeef7cb1bb846c4
Csaba Ekart <csaba.ekart@gmail.com>
2024-10-21 21:38:25 +0200

    objective at the beginning

--- globals.gd
+++ globals.gd
@@ -2,5 +2,5 @@ extends Node
 
 var inventory: Array[String]
 
-var current_objective: String = "speak with the grave digger"
+var current_objective: String = ""
 var player_movement_blocked: bool = false
