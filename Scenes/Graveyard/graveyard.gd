ff8df195885c4721bfcd7ce28e28a8a420b1cffd
Balazs <balzs.gyngysi@gmail.com>
2024-10-23 18:26:33 +0200

    sfx items added

--- Scenes/Graveyard/graveyard.gd
+++ Scenes/Graveyard/graveyard.gd
@@ -3,12 +3,13 @@ extends Node2D
 @onready var lantern: Node2D = $Lantern
 @onready var head: Node2D = $Head
 @onready var head_bloody: Node2D = $HeadBloody
-
+@export var sfx_item: AudioStreamPlayer
 signal head_collected_on_graveyard
 #signal head_bloody_collected_on_graveyard
 
 func _on_lantern_item_collected(item_name) -> void:
 	Globals.inventory.append(item_name)
+	sfx_item.play()
 	if !Globals.inventory.has("Head") && !Globals.inventory.has("HeadBloody") :
 		Globals.current_objective = "Bring the lantern to the grave digger"
 
@@ -21,6 +22,7 @@ func _on_show_head() -> void:
 	head.enableCollisionArea()
 
 func _on_head_item_collected(item) -> void:
+	sfx_item.play()
 	head_collected_on_graveyard.emit(item)
 
 #func _on_head_bloody_item_collected(item) -> void:
