5f8c635ec60026ad9a980864b15d877c1214ec57
Csaba Ekart <csaba.ekart@gmail.com>
2024-10-23 20:47:00 +0200

    delay added for button pressing

--- Scenes/Dialogue/dialogue_choice.gd
+++ Scenes/Dialogue/dialogue_choice.gd
@@ -2,14 +2,23 @@ extends CanvasLayer
 
 @onready var paragon: Button = $HBoxContainer/Paragon
 @onready var renegade: Button = $HBoxContainer/Renegade
+@onready var initial_timer: Timer = Timer.new()
 
 signal paragon_choice_made()
 signal renegade_choice_made()
 
+var can_use_interact := false  # Flag to control interact input
+
 func _ready() -> void:
 	paragon.grab_focus()
-	set_process_unhandled_input(false)  # Disable unhandled input
+	set_process_unhandled_input(false)
 	set_process_input(true)
+	
+	# Setup initial timer
+	add_child(initial_timer)
+	initial_timer.one_shot = true
+	initial_timer.timeout.connect(_on_initial_timer_timeout)
+	initial_timer.start(2.0)  # 2 second initial delay
 
 func _input(event: InputEvent) -> void:
 	# Stop the event from propagating to other nodes
@@ -21,11 +30,13 @@ func _input(event: InputEvent) -> void:
 			paragon.grab_focus()
 	
 	elif event.is_action_pressed("interact"):
-		get_viewport().set_input_as_handled()  # Prevent event from reaching DialogueManager
-		if paragon.has_focus():
-			_on_paragon_pressed()
-		elif renegade.has_focus():
-			_on_renegade_pressed()
+		# Only handle interact if the timer has finished
+		if can_use_interact:
+			get_viewport().set_input_as_handled()
+			if paragon.has_focus():
+				_on_paragon_pressed()
+			elif renegade.has_focus():
+				_on_renegade_pressed()
 
 func set_paragon_choice(paragon_choice_text: String) -> void:
 	paragon.text = paragon_choice_text
@@ -38,3 +49,6 @@ func _on_paragon_pressed() -> void:
 
 func _on_renegade_pressed() -> void:
 	renegade_choice_made.emit()
+
+func _on_initial_timer_timeout() -> void:
+	can_use_interact = true
