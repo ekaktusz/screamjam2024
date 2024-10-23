95c3347fb47efdadea6d922c4424f6fda426f2a2
Csaba Ekart <csaba.ekart@gmail.com>
2024-10-23 12:51:21 +0200

    reset globals

--- globals.gd
+++ globals.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-var inventory: Array[String] = ["Leg", "Head", "Torso", "Hands"]
+var inventory: Array[String] #= ["Leg", "Head", "Torso", "Hands"]
 
 var current_objective: String = ""
 var player_movement_blocked: bool = false
@@ -9,9 +9,7 @@ var player_movement_blocked: bool = false
 var hud_visible = false
 
 func reset_globals() -> void:
-	var inventory: Array[String]
-
-	var current_objective: String = ""
-	var player_movement_blocked: bool = false
-
-	var hud_visible = false
+	inventory.clear()
+	current_objective = ""
+	player_movement_blocked = false
+	hud_visible = false
