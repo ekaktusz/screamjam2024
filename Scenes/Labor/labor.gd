045f48280b70b8ae653d200e961b61f605587175
Csaba Ekart <csaba.ekart@gmail.com>
2024-10-23 09:28:03 +0200

    photointeraction

--- Scenes/Labor/labor.gd
+++ Scenes/Labor/labor.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+@onready var photo_interaction_area: InteractionArea = $PhotoInteractionArea
+@onready var collision_shape_2d: CollisionShape2D = $PhotoInteractionArea/CollisionShape2D
 signal torso_collected_in_labor
-@onready var collision_shape_2d: CollisionShape2D = $InteractionArea/CollisionShape2D
 
 func _ready() -> void:
+	photo_interaction_area.interact = Callable(self,"_on_photo_interact")
 	collision_shape_2d.disabled = true
 
 func _process(delta: float) -> void:
@@ -12,6 +14,9 @@ func _process(delta: float) -> void:
 	   (Globals.inventory.has("Leg") or Globals.inventory.has("LegBloody")) and
 	   (Globals.inventory.has("Torso") and collision_shape_2d.disabled)):
 		collision_shape_2d.disabled = false
+		
+func _on_photo_interact():
+	print("photo")
 
 func _on_item_collected(item) -> void:
 	torso_collected_in_labor.emit(item)
