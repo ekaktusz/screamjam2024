ac7f0b94fa049d4fafef225e5692dbe0a74ded75
Csaba Ekart <csaba.ekart@gmail.com>
2024-10-23 12:40:22 +0200

    hmmmmmmmmmmmmmmmmmmmmmmmmm

--- globals.gd
+++ globals.gd
@@ -1,8 +1,17 @@
 extends Node
 
-var inventory: Array[String]
+
+var inventory: Array[String] = ["Leg", "Head", "Torso", "Hands"]
 
 var current_objective: String = ""
 var player_movement_blocked: bool = false
 
 var hud_visible = false
+
+func reset_globals() -> void:
+	var inventory: Array[String]
+
+	var current_objective: String = ""
+	var player_movement_blocked: bool = false
+
+	var hud_visible = false
